9b431c4b138602b77a22585a5a255b918251b782
Cory Reed <swashcap@gmail.com>
2018-02-13 00:27:56 -0800

    feat(pkg): add font weight samples

--- App.js
+++ App.js
@@ -12,27 +12,46 @@ import {
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+/** {@link https://github.com/react-native-training/react-native-fonts#android */
+const names = Platform.OS === 'android' ?
+  [
+    'sans-serif',
+    'sans-serif-light',
+    'sans-serif-thin',
+    'sans-serif-condensed',
+    'sans-serif-medium',
+  ] :
+  [];
+const weights = [100, 200, 300, 400, 500, 600, 700, 800, 900];
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        {names.map((fontFamily) => (
+          <Text
+            key={fontFamily}
+            style={[
+              styles.text,
+              { fontFamily }
+            ]}
+          >
+            Hamburgfontsiv{` (${fontFamily})`}
+          </Text>
+        ))}
+
+        {weights.map((weight) => (
+          <Text
+            key={weight}
+            style={[
+              styles.text,
+              { fontWeight: weight.toString() },
+            ]}
+          >
+            Hamburgfonstiv{` (${weight})`}
+          </Text>
+        ))}
       </View>
     );
   }
@@ -43,16 +62,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'white',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  text: {
+    color: 'black',
+    marginVertical: 4,
   },
 });
